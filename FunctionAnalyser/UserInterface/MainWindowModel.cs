91c605ee71b901084e3629f0cbed6a94d5ee9d0b
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2020-10-04 00:41:37 +0200

    Added tooltips to options

--- FunctionAnalyser/UserInterface/MainWindowModel.cs
+++ FunctionAnalyser/UserInterface/MainWindowModel.cs
@@ -56,5 +56,9 @@ namespace UserInterface
                 StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(nameof(EnableOptions)));
             }
         }
+
+        public static string SkipFunctionOnErrorTooltip { get; } = "Skips the function if it contains a command error.\nIt will not contribute to the information found.";
+        public static string ShowCommandErrorsTooltip { get; } = "Shows command errors if they are found.\nA function may contain multiple errors.";
+        public static string ShowEmptyFunctionsTooltip { get; } = "Shows empty functions if they are found.\nA function is empty if it does not contain any commands.";
     }
 }
