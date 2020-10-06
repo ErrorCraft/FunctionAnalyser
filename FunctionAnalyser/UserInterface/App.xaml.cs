47f5268d174e78f61aec40bccf6e242bacf8e771
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2020-10-06 17:58:36 +0200

    Added exception window

--- FunctionAnalyser/UserInterface/App.xaml.cs
+++ FunctionAnalyser/UserInterface/App.xaml.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace UserInterface
 {
@@ -13,5 +15,28 @@ namespace UserInterface
     /// </summary>
     public partial class App : Application
     {
+        private void Crash(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            //string basePath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\";
+            string basePath = @"C:\Users\Ramon\Desktop\MCC and such\_test lol\";
+            string file = "FunctionAnalyser-Crash-" + DateTime.Now.ToString("yyyy-MM-dd_hh.mm.ss") + ".txt";
+
+            using (StreamWriter writer = new StreamWriter(basePath + file))
+            {
+                writer.WriteLine($"Time: {DateTime.Now:yyyy-MM-dd hh:mm:ss}");
+                writer.WriteLine($"Description: {e.Exception.Message}");
+                writer.WriteLine();
+                writer.WriteLine(e.Exception.GetType().FullName);
+                writer.WriteLine(e.Exception.StackTrace);
+            }
+
+            e.Handled = true;
+
+            CrashWindow crashWindow = new CrashWindow(e.Exception, basePath + file);
+            crashWindow.ShowDialog();
+            MainWindow.Close();
+
+            Shutdown(-1);
+        }
     }
 }
---
+++ FunctionAnalyser/UserInterface/CrashWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UserInterface
+{
+    public partial class CrashWindow : Window
+    {
+        private readonly string CrashPath;
+
+        public CrashWindow(Exception exception, string crashPath)
+        {
+            InitializeComponent();
+            CrashDescription.Text = $"{exception.GetType().FullName}: {exception.Message}";
+            CrashPath = crashPath;
+        }
+
+        private void ViewReport(object sender, RoutedEventArgs e)
+        {
+            new Process
+            {
+                StartInfo = new ProcessStartInfo(CrashPath)
+                {
+                    UseShellExecute = true
+                }
+            }.Start();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
