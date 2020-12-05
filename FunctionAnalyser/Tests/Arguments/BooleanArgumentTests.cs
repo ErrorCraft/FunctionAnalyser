a42844b3c6d6f5d5a6594ef4a381c39c57606930
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2020-12-05 23:42:08 +0100

    Add unit tests
    I should've done this way earlier...

---
+++ FunctionAnalyser/Tests/Arguments/BooleanArgumentTests.cs
@@ -0,0 +1,53 @@
+﻿using CommandParser;
+using CommandParser.Arguments;
+using CommandParser.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Arguments
+{
+    [TestClass]
+    public class BooleanArgumentTests
+    {
+        [TestMethod]
+        public void BooleanArgument_ParseShouldSucceed()
+        {
+            // Arrange
+            BooleanArgument argument = new BooleanArgument();
+            StringReader reader = new StringReader("true");
+
+            // Act
+            ReadResults readResults = argument.Parse(reader, out _);
+
+            // Assert
+            Assert.IsTrue(readResults.Successful);
+        }
+
+        [TestMethod]
+        public void BooleanArgument_ParseShouldFail()
+        {
+            // Arrange
+            BooleanArgument argument = new BooleanArgument();
+            StringReader reader = new StringReader("hello");
+
+            // Act
+            ReadResults readResults = argument.Parse(reader, out _);
+
+            // Assert
+            Assert.IsFalse(readResults.Successful);
+        }
+
+        [TestMethod]
+        public void BooleanArgument_UseQuotedText()
+        {
+            // Arrange
+            BooleanArgument argument = new BooleanArgument();
+            StringReader reader = new StringReader("'true'");
+
+            // Act
+            ReadResults readResults = argument.Parse(reader, out bool result);
+
+            // Assert
+            Assert.AreEqual(result, true);
+        }
+    }
+}
