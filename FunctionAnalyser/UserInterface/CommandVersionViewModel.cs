649ded2601b962ed42e000016ce322e031733d1b
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2020-12-19 16:40:32 +0100

    Add start for command versions in UI

---
+++ FunctionAnalyser/UserInterface/CommandVersionViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace UserInterface
+{
+    public class CommandVersionViewModel
+    {
+        private readonly string CommandName;
+        private readonly string FancyName;
+
+        public CommandVersionViewModel(string commandName, string fancyName)
+        {
+            CommandName = commandName;
+            FancyName = fancyName;
+        }
+
+        public string GetCommandName()
+        {
+            return CommandName;
+        }
+
+        public override string ToString()
+        {
+            return FancyName;
+        }
+    }
+}
