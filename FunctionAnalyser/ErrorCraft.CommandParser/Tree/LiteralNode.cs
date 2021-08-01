68553e31795d1b07663df564e3e1646f6eb088c1
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-01 22:59:57 +0200

    Add LiteralNode

---
+++ FunctionAnalyser/ErrorCraft.CommandParser/Tree/LiteralNode.cs
@@ -0,0 +1,13 @@
+﻿namespace ErrorCraft.CommandParser.Tree {
+    public class LiteralNode : Node {
+        private readonly string Literal;
+
+        public LiteralNode(string literal) {
+            Literal = literal;
+        }
+
+        public override string GetName() {
+            return Literal;
+        }
+    }
+}
