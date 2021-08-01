3c635e086ba1695f853aea27218dacc179617dd5
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-01 22:57:06 +0200

    Add abstract Node class

---
+++ FunctionAnalyser/ErrorCraft.CommandParser/Tree/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace ErrorCraft.CommandParser.Tree {
+    public abstract class Node {
+        private readonly Dictionary<string, Node> Children = new Dictionary<string, Node>();
+
+        public abstract string GetName();
+
+        public void AddChild(Node node) {
+            Children.Add(node.GetName(), node);
+        }
+
+        public IEnumerable<Node> GetChildren() {
+            foreach (Node child in Children.Values) {
+                yield return child;
+            }
+        }
+    }
+}
