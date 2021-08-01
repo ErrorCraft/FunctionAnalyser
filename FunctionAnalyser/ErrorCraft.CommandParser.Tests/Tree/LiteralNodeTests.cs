f599c2293f7c19aa95e8695fc3f611e4b1a5e897
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-01 23:05:45 +0200

    Add LiteralNode unit tests

---
+++ FunctionAnalyser/ErrorCraft.CommandParser.Tests/Tree/LiteralNodeTests.cs
@@ -0,0 +1,14 @@
+﻿using ErrorCraft.CommandParser.Tree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ErrorCraft.CommandParser.Tests.Tree {
+    [TestClass]
+    public class LiteralNodeTests {
+        [TestMethod]
+        public void GetName_ReturnsLiteral() {
+            LiteralNode literalNode = new LiteralNode("foo");
+            string name = literalNode.GetName();
+            Assert.AreEqual("foo", name);
+        }
+    }
+}
