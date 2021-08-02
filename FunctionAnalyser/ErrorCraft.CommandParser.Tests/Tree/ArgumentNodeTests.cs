cc7da0a5d514e4cc5bed0213a852bcb42509d201
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-02 22:59:54 +0200

    Add ArgumentNode

---
+++ FunctionAnalyser/ErrorCraft.CommandParser.Tests/Tree/ArgumentNodeTests.cs
@@ -0,0 +1,14 @@
+﻿using ErrorCraft.CommandParser.Tree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ErrorCraft.CommandParser.Tests.Tree {
+    [TestClass]
+    public class ArgumentNodeTests {
+        [TestMethod]
+        public void GetName_ReturnsCorrectValue() {
+            ArgumentNode<int> argumentNode = new ArgumentNode<int>("foo");
+            string name = argumentNode.GetName();
+            Assert.AreEqual("foo", name);
+        }
+    }
+}
---
+++ FunctionAnalyser/ErrorCraft.CommandParser/Tree/ArgumentNode.cs
@@ -0,0 +1,13 @@
+﻿namespace ErrorCraft.CommandParser.Tree {
+    public class ArgumentNode<T> : Node {
+        private readonly string Name;
+
+        public ArgumentNode(string name) {
+            Name = name;
+        }
+
+        public override string GetName() {
+            return Name;
+        }
+    }
+}
