e04f2ff8d06a571d642b1c8fafa14a68f9cbf690
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-03 00:06:19 +0200

    Add CommandError

---
+++ FunctionAnalyser/ErrorCraft.CommandParser/CommandError.cs
@@ -0,0 +1,13 @@
+﻿namespace ErrorCraft.CommandParser {
+    public class CommandError {
+        private string Message;
+
+        public CommandError(string message) {
+            Message = message;
+        }
+
+        public string GetMessage() {
+            return Message;
+        }
+    }
+}
