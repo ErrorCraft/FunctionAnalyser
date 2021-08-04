7ebca39603dfb2cab741eb571cf2d40c1a93b44e
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-04 23:36:15 +0200

    Add IntegerArgument

---
+++ FunctionAnalyser/ErrorCraft.CommandParser.Tests/Arguments/IntegerArgumentTests.cs
@@ -0,0 +1,32 @@
+﻿using ErrorCraft.CommandParser.Arguments;
+using ErrorCraft.CommandParser.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ErrorCraft.CommandParser.Tests.Arguments {
+    [TestClass]
+    public class IntegerArgumentTests {
+        [TestMethod]
+        public void Parse_ReturnsCorrectValue() {
+            IntegerArgument integerArgument = new IntegerArgument();
+            StringReader stringReader = new StringReader("100");
+            integerArgument.Parse(stringReader, out int result);
+            Assert.AreEqual(100, result);
+        }
+
+        [TestMethod]
+        public void Parse_IsUnsuccessful_BecauseStringIsEmpty() {
+            IntegerArgument integerArgument = new IntegerArgument();
+            StringReader stringReader = new StringReader("");
+            ParseResults parseResults = integerArgument.Parse(stringReader, out _);
+            Assert.IsFalse(parseResults.Successful);
+        }
+
+        [TestMethod]
+        public void Parse_IsUnsuccessful_BecauseIntegerIsInvalid() {
+            IntegerArgument integerArgument = new IntegerArgument();
+            StringReader stringReader = new StringReader("--1");
+            ParseResults parseResults = integerArgument.Parse(stringReader, out _);
+            Assert.IsFalse(parseResults.Successful);
+        }
+    }
+}
---
+++ FunctionAnalyser/ErrorCraft.CommandParser/Arguments/IntegerArgument.cs
@@ -0,0 +1,9 @@
+﻿using ErrorCraft.CommandParser.Results;
+
+namespace ErrorCraft.CommandParser.Arguments {
+    public class IntegerArgument : IArgument<int> {
+        public ParseResults Parse(IStringReader reader, out int result) {
+            return reader.ReadInteger(out result);
+        }
+    }
+}
