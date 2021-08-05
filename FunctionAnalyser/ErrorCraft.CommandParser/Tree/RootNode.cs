a95034dc8ceaf2b21693427bd1e8cb9af9d5a223
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-05 16:33:50 +0200

    Add RootNode

---
+++ FunctionAnalyser/ErrorCraft.CommandParser/Tree/RootNode.cs
@@ -0,0 +1,14 @@
+﻿using ErrorCraft.CommandParser.Results;
+using System;
+
+namespace ErrorCraft.CommandParser.Tree {
+    public class RootNode : Node {
+        public override string GetName() {
+            return "";
+        }
+
+        public override ParseResults Parse(IStringReader reader) {
+            throw new NotSupportedException();
+        }
+    }
+}
