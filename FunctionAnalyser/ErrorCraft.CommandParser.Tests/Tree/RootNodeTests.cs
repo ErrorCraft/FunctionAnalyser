ef0efa3d69a6283cfdd216e888938e99eb127743
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2021-08-05 16:42:50 +0200

    Add RootNode unit tests

---
+++ FunctionAnalyser/ErrorCraft.CommandParser.Tests/Tree/RootNodeTests.cs
@@ -0,0 +1,22 @@
+﻿using ErrorCraft.CommandParser.Tree;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ErrorCraft.CommandParser.Tests.Tree {
+    [TestClass]
+    public class RootNodeTests {
+        [TestMethod]
+        public void GetName_ReturnsEmptyString() {
+            RootNode rootNode = new RootNode();
+            string name = rootNode.GetName();
+            Assert.AreEqual("", name);
+        }
+
+        [TestMethod]
+        public void Parse_ThrowsException() {
+            RootNode rootNode = new RootNode();
+            IStringReader stringReader = new StringReader("");
+            Assert.ThrowsException<NotSupportedException>(() => { rootNode.Parse(stringReader); });
+        }
+    }
+}
