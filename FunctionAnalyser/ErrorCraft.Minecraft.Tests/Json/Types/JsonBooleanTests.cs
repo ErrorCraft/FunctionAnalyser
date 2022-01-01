bbdc8f84dfd1f38332fd2239aa09453c8a1f8e54
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2022-01-01 15:17:02 +0100

    Add JsonBoolean TryParse tests

--- FunctionAnalyser/ErrorCraft.Minecraft.Tests/Json/Types/JsonBooleanTests.cs
+++ FunctionAnalyser/ErrorCraft.Minecraft.Tests/Json/Types/JsonBooleanTests.cs
@@ -10,4 +10,25 @@ public class JsonBooleanTests {
         JsonBoolean json = new JsonBoolean(false);
         Assert.AreEqual(JsonElementType.BOOLEAN, json.GetElementType());
     }
+
+    [TestMethod]
+    public void TryParse_WithTrue_ReturnsCorrectValue() {
+        string s = "true";
+        _ = JsonBoolean.TryParse(s, out JsonBoolean? result);
+        Assert.AreEqual(true, (bool)result);
+    }
+
+    [TestMethod]
+    public void TryParse_WithFalse_ReturnsCorrectValue() {
+        string s = "false";
+        _ = JsonBoolean.TryParse(s, out JsonBoolean? result);
+        Assert.AreEqual(false, (bool)result);
+    }
+
+    [TestMethod]
+    public void TryParse_ReturnsFalse_BecauseInputIsInvalid() {
+        string s = "invalid";
+        bool successful = JsonBoolean.TryParse(s, out _);
+        Assert.IsFalse(successful);
+    }
 }
