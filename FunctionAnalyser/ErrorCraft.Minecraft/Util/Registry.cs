68546211a428167299fbc630804c0e7978f83935
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2022-01-04 18:00:56 +0100

    Add Registry

---
+++ FunctionAnalyser/ErrorCraft.Minecraft/Util/Registry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ErrorCraft.Minecraft.Util;
+
+internal class Registry<T> {
+    private readonly Dictionary<ResourceLocation, T> Items = new Dictionary<ResourceLocation, T>();
+
+    public T this[ResourceLocation resourceLocation] {
+        get { return Items[resourceLocation]; }
+    }
+
+    public T Register(ResourceLocation resourceLocation, T value) {
+        Items[resourceLocation] = value;
+        return value;
+    }
+}
