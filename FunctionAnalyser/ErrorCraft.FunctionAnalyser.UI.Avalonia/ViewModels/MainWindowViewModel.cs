6f043a74221ae5e1190b88127c1e841f2893ac81
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2022-09-07 21:24:35 +0200

    Add Avalonia UI project

---
+++ FunctionAnalyser/ErrorCraft.FunctionAnalyser.UI.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ErrorCraft.FunctionAnalyser.UI.Avalonia.ViewModels;
+public class MainWindowViewModel : ViewModelBase {
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ FunctionAnalyser/ErrorCraft.FunctionAnalyser.UI.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,8 @@
+using Avalonia.Controls;
+
+namespace ErrorCraft.FunctionAnalyser.UI.Avalonia.Views;
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+    }
+}
