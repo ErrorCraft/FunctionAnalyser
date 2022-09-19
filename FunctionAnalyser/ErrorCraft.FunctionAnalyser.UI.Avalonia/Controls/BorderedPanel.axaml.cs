0e23eb8b7a333834c9e064fd1fdcd9b9dffd1f10
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2022-09-19 23:28:56 +0200

    Add background and shadow colours to BorderedPanel

--- FunctionAnalyser/ErrorCraft.FunctionAnalyser.UI.Avalonia/Controls/BorderedPanel.axaml.cs
+++ FunctionAnalyser/ErrorCraft.FunctionAnalyser.UI.Avalonia/Controls/BorderedPanel.axaml.cs
@@ -1,6 +1,26 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace ErrorCraft.FunctionAnalyser.UI.Avalonia.Controls;
 
 public class BorderedPanel : ItemsControl {
+    public static readonly StyledProperty<IBrush?> BackgroundColourProperty = AvaloniaProperty.Register<BorderedPanel, IBrush?>(nameof(BackgroundColour));
+    public static readonly StyledProperty<IBrush?> ShadowColourProperty = AvaloniaProperty.Register<BorderedPanel, IBrush?>(nameof(ShadowColour));
+    public static readonly StyledProperty<bool> HasEdgeProperty = AvaloniaProperty.Register<BorderedPanel, bool>(nameof(HasEdge), false);
+
+    public IBrush? BackgroundColour {
+        get { return GetValue(BackgroundColourProperty); }
+        set { SetValue(BackgroundColourProperty, value); }
+    }
+
+    public IBrush? ShadowColour {
+        get { return GetValue(ShadowColourProperty); }
+        set { SetValue(ShadowColourProperty, value); }
+    }
+
+    public bool HasEdge {
+        get { return GetValue(HasEdgeProperty); }
+        set { SetValue(HasEdgeProperty, value); }
+    }
 }
---
+++ FunctionAnalyser/ErrorCraft.FunctionAnalyser.UI.Avalonia/Converters/BorderedPanelBoxShadowConverter.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media;
+
+namespace ErrorCraft.FunctionAnalyser.UI.Avalonia.Converters;
+
+public static class BorderedPanelBoxShadowConverter {
+    public static readonly IValueConverter Converter = new FuncValueConverter<ISolidColorBrush?, BoxShadows>(Convert);
+
+    private static BoxShadows Convert(ISolidColorBrush? value) {
+        Color colour = value?.Color ?? Colors.Black;
+        BoxShadow boxShadow = new BoxShadow() {
+            IsInset = true,
+            OffsetX = 0.0d,
+            OffsetY = 1.0d,
+            Blur = 5.0d,
+            Color = colour
+        };
+        return new BoxShadows(boxShadow);
+    }
+}
